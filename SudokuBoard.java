de7d50443d83eef594f90a29e088df7164cfb62b
Eli <eli@currah.com>
2025-04-16 12:15:29 -0700

    Got started on constructor, toString, and main

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,9 +1,32 @@
-public class SudokuBoard{
-private char[][] board = new char[9][9];
-  public SudokuBoard(String s){
+import java.util.*;
 
+public class SudokuBoard{
+private char[][] board;
+
+  public SudokuBoard(String fileName){
+    board = new char[9][9];
+    try {
+      Scanner file = new Scanner(new File(fileName));
+
+      for (int r = 0; r < grid.length; r++) {
+          for (int c = 0; c < board[0].length; c++) {
+              board[r][c] = file.nextChar();
+          }
+      }
+
+  } catch (FileNotFoundException e) {
+      System.out.println("Cannot load: " + fileName);
+  } catch (InputMismatchException e) {
+      System.out.println(fileName + " does not meet format expectations.");
   }
+  }
+
   public String toString(){
-    return "";
+    return board;
+  }
+
+  public static void main(String[] args){
+    SudokuBoard sudokuBoard = new SudokuBoard("data1.sdk");
+    System.out.print(sudokuBoard);
   }
 }
